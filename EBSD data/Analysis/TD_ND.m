b7c0884b5b97848427011c2b6d774b5323e98876
Peter Crowther <13117069+merrygoat@users.noreply.github.com>
2020-05-04 15:23:18 +0100

    Initial commit

---
+++ EBSD data/Analysis/TD_ND.m
@@ -0,0 +1,59 @@
+%% Import Script for EBSD Data
+%
+% This script was automatically created by the import wizard. You should
+% run the whoole script or parts of it in order to import your data. There
+% is no problem in making any changes to this script.
+
+%% Specify Crystal and Specimen Symmetries
+
+% crystal symmetry
+CS = {... 
+  'notIndexed',...
+  crystalSymmetry('m-3m', [4.05 4.05 4.05], 'mineral', 'Aluminium', 'color', 'light blue')};
+
+% plotting convention
+setMTEXpref('xAxisDirection','east');
+setMTEXpref('zAxisDirection','outofplane');
+
+%% Specify File Names
+
+% path to files
+pname = 'H:\Surfalex_All data\EBSD data\Data\TD-ND plane';
+
+% which files to be imported
+fname = [pname '\TD-ND Data.ctf'];
+
+%% Import the Data
+
+% create an EBSD variable containing the data
+ebsd = loadEBSD(fname,CS,'interface','ctf',...
+  'convertEuler2SpatialReferenceFrame');
+
+rot = rotation('axis', xvector, 'angle',90*degree);
+ebsd = rotate(ebsd, rot);
+
+ebsd = ebsd('indexed');
+[grains, ebsd.grainId] = calcGrains(ebsd,'angle',5*degree);
+grains = smooth(grains);
+
+plot(ebsd,ebsd.bc);
+colormap gray
+mtexColorbar;
+
+hold on
+
+oM = ipfHSVKey(ebsd('indexed'))
+oM.inversePoleFigureDirection = xvector;
+color = oM.orientation2color(ebsd('indexed').orientations);
+plot(ebsd('indexed'),color);
+
+hold off
+
+figure ()
+
+ori=ebsd('Aluminium').orientations
+x=[Miller(1,1,1,ori.CS),Miller(2,0,0,ori.CS),Miller(2,2,0,ori.CS)]; % include hkil figures here
+plotPDF(ori,x,'antipodal','contourf','colorrange',[1 3.5])
+colorbar;
+
+
---
+++ Forming test data/Analysis/FLC_necking.m
@@ -0,0 +1,98 @@
+clear;
+clc;
+
+cooktemp=[1,3,5, 8, 10, 12, 15, 17, 19]; 
+cooksteps=9;
+cookstepcount=1;
+
+
+% importing all 10 mm data
+raw_10mm = xlsread('Strain path data_necking.xlsx','Sheet1','B4:U128');
+
+
+% importing all 20 mm data
+raw_20mm = xlsread('Strain path data_necking.xlsx','Sheet2','B4:U121');
+
+ 
+% % importing all 40 mm data
+raw_40mm = xlsread('Strain path data_necking.xlsx','Sheet3','B4:U131');
+
+
+% importing all 60 mm data
+raw_60mm = xlsread('Strain path data_necking.xlsx','Sheet4','B4:U129');
+
+
+% % importing all 120 mm data
+raw_120mm = xlsread('Strain path data_necking.xlsx','Sheet5','B4:U127');
+
+
+
+ % importing all 177 mm data
+raw_177mm = xlsread('Strain path data_necking.xlsx','Sheet6','B4:U137');
+
+
+while cookstepcount<=cooksteps 
+    scatter(raw_10mm(:,cooktemp(cookstepcount)),raw_10mm(:,cooktemp(cookstepcount)+1))
+    hold on
+    cookstepcount=cookstepcount+1;
+    
+end
+
+cookstepcount=1;
+
+hold on
+while cookstepcount<=cooksteps 
+    scatter(raw_20mm(:,cooktemp(cookstepcount)),raw_20mm(:,cooktemp(cookstepcount)+1))
+    hold on
+    cookstepcount=cookstepcount+1;
+    
+end
+cookstepcount=1;
+
+hold on
+while cookstepcount<=cooksteps 
+    scatter(raw_40mm(:,cooktemp(cookstepcount)),raw_40mm(:,cooktemp(cookstepcount)+1))
+    hold on
+    cookstepcount=cookstepcount+1;
+    
+end
+cookstepcount=1;
+
+hold on
+while cookstepcount<=cooksteps 
+    scatter(raw_60mm(:,cooktemp(cookstepcount)),raw_60mm(:,cooktemp(cookstepcount)+1))
+    hold on
+    cookstepcount=cookstepcount+1;
+    
+end
+cookstepcount=1;
+
+hold on
+while cookstepcount<=cooksteps 
+    scatter(raw_120mm(:,cooktemp(cookstepcount)),raw_120mm(:,cooktemp(cookstepcount)+1))
+    hold on
+    cookstepcount=cookstepcount+1;
+    
+end
+cookstepcount=1;
+
+hold on
+while cookstepcount<=cooksteps 
+    scatter(raw_177mm(:,cooktemp(cookstepcount)),raw_177mm(:,cooktemp(cookstepcount)+1))
+    hold on
+    cookstepcount=cookstepcount+1;
+    
+end
+
+set(gca,'FontSize',30,'fontweight','bold')
+set(gcf,'color','w');
+set(gca,'linewidth',3)
+xlabel('Minor strain)','fontweight','bold','fontsize',32)
+ylabel('Major strain','fontweight','bold','fontsize',32)
+axis([-0.2 0.2 0 0.35])
+box on
+plot([0 0],get(gca,'YLim'),'k','linewidth',3);
+set(gcf, 'units','normalized','outerposition',[0 0 1 1]); %Maximize figure.
+print(gcf,'FLC_necking.svg','-dsvg','-r600');
+
+
